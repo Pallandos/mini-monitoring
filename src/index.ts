6275713ac742fea9e402efd827911ed860f7c3a7
Pallandos <jean.caylus@ensta.fr>
2025-11-01 14:00:59 +0100

    feat: add env var for port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { app } from './monitoring';
 
 // importe et LANCE monitoring.ts
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Monitoring server is live on http://localhost:${port}`);
 });
