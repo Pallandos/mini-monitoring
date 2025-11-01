20ef18026af93aed8b0becb8d4c1e0f7799a446f
Pallandos <jean.caylus@ensta.fr>
2025-11-01 22:52:23 +0100

    fix: change port var name

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { app } from './monitoring';
 
 // importe et LANCE monitoring.ts
 
-const port = process.env.PORT || 3000;
+const port = process.env.APP_PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Monitoring server is live on http://localhost:${port}`);
